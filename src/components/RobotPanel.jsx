7915e4c45d7848011b5d577c62ebfc0e0e2100e5
Saeid Kabak <saeedkabak@gmail.com>
2026-09-24 21:59:59 +0200

    Keep robot scene interactive

--- src/components/RobotPanel.jsx
+++ src/components/RobotPanel.jsx
@@ -4,7 +4,7 @@ const Spline = lazy(() => import("@splinetool/react-spline"));
 
 export default function RobotPanel() {
   return (
-    <div className="contact-robot" aria-hidden="true">
+    <div className="contact-robot">
       <Suspense fallback={<div className="contact-robot-loader" />}>
         <Spline scene="https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode" className="contact-robot-scene" />
       </Suspense>
